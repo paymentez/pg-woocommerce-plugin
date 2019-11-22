a39bc0db5f738b5a59dbab7fa1362be535b157f8
sergioevl <svargas@paymentez.com>
2019-11-21 18:09:35 -0600

    Primer commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# pg-woocommerce-plugin
---
+++ pg-woocommerce-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: PG Woocommerce Plugin
+Plugin URI: http://www.paymentez.com
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Paymentez
+Author URI: http://www.paymentez.com
+License: A "Slug" license name e.g. GPL2
+*/
