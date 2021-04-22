01fd4defe535512997bf3a22ea4be8fae1039be7
sergioevl <sergioevl@github.com>
2021-04-22 10:06:01 -0500

    Ajuste al webhook

--- includes/pg-woocommerce-helper.php
+++ includes/pg-woocommerce-helper.php
@@ -3,179 +3,203 @@ require_once( dirname( __DIR__ ) . '/pg-woocommerce-plugin.php' );
 
 global $wpdb;
 
-define("TABLE_NAME", $wpdb->prefix . 'pg_wc_plugin');
+define("PG_TABLE_NAME", $wpdb->prefix . 'pg_wc_plugin');
 
 /**
- *
+ * Helper class to manage actions from the payment gateway and actions on database.
  */
 class PG_WC_Helper
 {
 
-  /**
-   *
-   */
-  public static function create_table() {
-    global $wpdb;
+    /**
+     * Creates the required table on Wordpress database.
+     */
+    public static function create_table() {
+        global $wpdb;
 
-    $sql = 'CREATE TABLE '.TABLE_NAME.' (
-           id integer(9) unsigned NOT NULL AUTO_INCREMENT,
-           status varchar(50) NOT NULL,
-           comments varchar(50) NOT NULL,
-           description text(500) NOT NULL,
-           order_id int(9) NOT NULL,
-           pg_transaction_id varchar(50) NOT NULL,
-           PRIMARY KEY  (id)
-           );';
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
+        $sql = 'CREATE TABLE '.PG_TABLE_NAME.' (
+               id integer(9) unsigned NOT NULL AUTO_INCREMENT,
+               status varchar(50) NOT NULL,
+               comments varchar(50) NOT NULL,
+               description text(500) NOT NULL,
+               order_id int(9) NOT NULL,
+               pg_transaction_id varchar(50) NOT NULL,
+               PRIMARY KEY  (id)
+               );';
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
 
-  }
-
-  /**
-   *
-   */
-  public static function insert_data($status, $comments, $description, $dev_reference, $transaction_id) {
-    global $wpdb;
-    $wpdb->insert(
-      TABLE_NAME,
-      array(
-        'status'            => $status,
-        'comments'          => $comments,
-        'description'       => $description,
-        'order_id'          => $dev_reference,
-        'pg_transaction_id' => $transaction_id
-      ),
-      array(
-        '%s',
-        '%s',
-        '%s',
-        '%s',
-        '%s'
-      )
-    );
-  }
-
-  /**
-   *
-   */
-  public static function select_order($order_id) {
-    global $wpdb;
-    $myrows = $wpdb->get_results("SELECT * FROM ".TABLE_NAME." where order_id = '$order_id' ", OBJECT);
-
-    foreach ($myrows as $campos) {
-      $transactionCode = $campos->pg_transaction_id;
     }
-    return $transactionCode;
-  }
 
-  /**
-   *
-   */
-  public static function get_checkout_params($order) {
-    $order_data = $order->get_data();
-
-    $description = "";
-    foreach ($order->get_items() as $product) {
-      $description .= $product['name'] . ',';
+    /**
+     * Inserts data to the plugin table.
+     */
+    public static function insert_data($status, $comments, $description, $dev_reference, $transaction_id) {
+        global $wpdb;
+        $wpdb->insert(
+            PG_TABLE_NAME,
+            array(
+                'status'            => $status,
+                'comments'          => $comments,
+                'description'       => $description,
+                'order_id'          => $dev_reference,
+                'pg_transaction_id' => $transaction_id
+            ),
+            array(
+                '%s',
+                '%s',
+                '%s',
+                '%s',
+                '%s'
+            )
+        );
     }
-    if (strlen($description) > 240) {
+
+    /**
+     * Gets a payment gateway transaction code for a Woocommerce order id.
+     * @param string $order_id
+     * @return string
+     */
+    public static function select_order($order_id) {
+        global $wpdb;
+        $myrows = $wpdb->get_results("SELECT * FROM ".PG_TABLE_NAME." where order_id = '$order_id' ", OBJECT);
+
+        foreach ($myrows as $campos) {
+            $transactionCode = $campos->pg_transaction_id;
+        }
+        return $transactionCode;
+    }
+
+    /**
+     * Method that build the required params to initialize the payment checkout.
+     * @param WC_Order $order
+     * @return array
+     */
+    public static function get_checkout_params($order) {
+        $order_data = $order->get_data();
+
+        $description = '';
+        foreach ($order->get_items() as $product) {
+            $description .= $product['name'] . ',';
+        }
+        if (strlen($description) > 240) {
             $description = substr($description,0,240);
+        }
+
+        if (is_null($order_data['customer_id']) or empty($order_data['customer_id'])) {
+            $uid = $order->id;
+        } else {
+            $uid = $order_data['customer_id'];
+        }
+
+        $vat = number_format(($order->get_total_tax()), 2, '.', '');
+
+        $parametersArgs = array(
+            'purchase_order_id'    => $order->get_id(),
+            'purchase_amount'      => $order_data['total'],
+            'purchase_description' => $description,
+            'customer_phone'       => $order_data['billing']['phone'],
+            'customer_email'       => $order_data['billing']['email'],
+            'user_id'              => $uid,
+            'vat'                  => $vat
+        );
+
+        return $parametersArgs;
     }
 
-    if (is_null($order_data['customer_id']) or empty($order_data['customer_id'])) {
-        $uid = $orderId;
-    } else {
-        $uid = $order_data['customer_id'];
+    /**
+     * Method to manage the LinkToPay request.
+     * @param WC_Order $order
+     * @param string $environment
+     * @return string URL to LinkToPay
+     */
+    public static function generate_ltp($order, $environment) {
+        $url_ltp = ($environment == 'yes') ? 'https://noccapi-stg.'.PG_DOMAIN.PG_LTP : 'https://noccapi.'.PG_DOMAIN.PG_LTP ;
+        $auth_token = PG_WC_Helper::generate_auth_token('server');
+
+        $checkout_data = PG_WC_Helper::get_checkout_params($order);
+        $redirect_url = $order->get_view_order_url();
+
+        $data = [
+            'user' => [
+                'id'=> $checkout_data['user_id'],
+                'name'=> $order->get_billing_first_name(),
+                'last_name'=> $order->get_billing_last_name(),
+                'email'=> $checkout_data['customer_email'],
+            ],
+            'order' => [
+                'dev_reference' => $checkout_data['purchase_order_id'],
+                'description' => $checkout_data['purchase_description'],
+                'amount' => $checkout_data['purchase_amount'],
+                'installments_type' => -1,
+                'currency' => $order->get_currency(),
+                'vat' => $checkout_data['vat'],
+            ],
+            'configuration' => [
+                'partial_payment' => false,
+                'expiration_days' => 1,
+                'success_url' => $redirect_url,
+                'failure_url' => $redirect_url,
+                'pending_url' => $redirect_url,
+                'review_url' => $redirect_url,
+            ]
+        ];
+
+        $payload = json_encode($data);
+
+        $ch = curl_init($url_ltp);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ($payload));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Auth-Token:' . $auth_token));
+
+        try {
+            $response = curl_exec($ch);
+            $get_response = json_decode($response, true);
+            $payment_url = $get_response['data']['payment']['payment_url'];
+        } catch (Exception $e) {
+            $payment_url = NULL;
+        }
+        curl_close($ch);
+        return $payment_url;
     }
 
-    $vat = number_format(($order->get_total_tax()), 2, '.', '');
+    /**
+     * Method to generate the payment gateway token for authentication.
+     * @param string $type server
+     * @return string|void
+     */
+    public static function generate_auth_token($type) {
+        $plugin = new PG_WC_Plugin();
+        if ($type == 'server') {
+            $app_code = $plugin->app_code_server;
+            $app_key = $plugin->app_key_server;
+        } elseif ($type == 'client') {
+            $app_code = $plugin->app_code_client;
+            $app_key = $plugin->app_key_client;
+        } else {
+            return;
+        }
 
-    $parametersArgs = array(
-      'purchase_order_id'    => $order->get_id(),
-      'purchase_amount'      => $order_data['total'],
-      'purchase_description' => $description,
-      'customer_phone'       => $order_data['billing']['phone'],
-      'customer_email'       => $order_data['billing']['email'],
-      'user_id'              => $uid,
-      'vat'                  => $vat
-    );
+        $timestamp = (string)(time());
+        $token_string = $app_key . $timestamp;
+        $token_hash = hash('sha256', $token_string);
 
-    return $parametersArgs;
-  }
-
-  /**
-   *
-   */
-  public static function generate_ltp($order, $environment) {
-    $url_ltp = ($environment == 'yes') ? 'https://noccapi-stg.'.PG_DOMAIN.PG_LTP : 'https://noccapi.'.PG_DOMAIN.PG_LTP ;
-    $auth_token = PG_WC_Helper::generate_auth_token('server');
-
-    $checkout_data = PG_WC_Helper::get_checkout_params($order);
-    $redirect_url = $order->get_view_order_url();
-
-    $data = [
-      'user' => [
-        'id'=> $checkout_data['user_id'],
-        'name'=> $order->get_billing_first_name(),
-        'last_name'=> $order->get_billing_last_name(),
-        'email'=> $checkout_data['customer_email'],
-      ],
-      'order' => [
-        'dev_reference' => $checkout_data['purchase_order_id'],
-        'description' => $checkout_data['purchase_description'],
-        'amount' => $checkout_data['purchase_amount'],
-        'installments_type' => -1,
-        'currency' => $order->get_currency(),
-        'vat' => $checkout_data['vat'],
-      ],
-      'configuration' => [
-        'partial_payment' => false,
-        'expiration_days' => 1,
-        'success_url' => $redirect_url,
-        'failure_url' => $redirect_url,
-        'pending_url' => $redirect_url,
-        'review_url' => $redirect_url,
-      ]
-    ];
-
-    $payload = json_encode($data);
-
-    $ch = curl_init($url_ltp);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ($payload));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Auth-Token:' . $auth_token));
-
-    try {
-      $response = curl_exec($ch);
-      $get_response = json_decode($response, true);
-      $payment_url = $get_response['data']['payment']['payment_url'];
-    } catch (Exception $e) {
-      $payment_url = NULL;
+        return base64_encode($app_code . ';' . $timestamp . ';' . $token_hash);
     }
-    curl_close($ch);
-    return $payment_url;
-  }
 
-  /**
-   *
-   */
-   public static function generate_auth_token($type) {
-     $plugin = new PG_WC_Plugin();
-     if ($type == 'server') {
-       $app_code = $plugin->app_code_server;
-       $app_key = $plugin->app_key_server;
-     } elseif ($type == 'client') {
-       $app_code = $plugin->app_code_client;
-       $app_key = $plugin->app_key_client;
-     }
+    /**
+     * Method to calculate the payment gateway stokens used for authentication.
+     * @param string $user_id
+     * @param string $transaction_id
+     * @return array containing the stokens for client and server credentials.
+     */
+    public static function get_stokens($user_id, $transaction_id)
+    {
+        $webhookObj = new PG_WC_Plugin();
+        $stoken_client = md5($transaction_id . "_" . $webhookObj->app_code_client . "_" . $user_id . "_" . $webhookObj->app_key_client);
+        $stoken_server = md5($transaction_id . "_" . $webhookObj->app_code_server . "_" . $user_id . "_" . $webhookObj->app_key_server);
+        return array($stoken_server, $stoken_client);
 
-     $fecha_actual = time();
-     $variableTimestamp = (string)($fecha_actual);
-     $uniq_token_string = $app_key . $variableTimestamp;
-     $uniq_token_hash = hash('sha256', $uniq_token_string);
-     $auth_token = base64_encode($app_code . ';' . $variableTimestamp . ';' . $uniq_token_hash);
-
-     return $auth_token;
-   }
+    }
 }
---
+++ includes/pg-woocommerce-webhook-checkout.php
@@ -0,0 +1,50 @@
+<?php
+require_once('../../../../wp-load.php');
+require_once( dirname( __FILE__ ) . '/pg-woocommerce-helper.php' );
+require_once( dirname( __DIR__ ) . '/pg-woocommerce-plugin.php' );
+
+$requestBodyJs = json_decode(file_get_contents('php://input'), true);
+
+$status = $requestBodyJs["transaction"]['status'];
+$status_detail = (int)$requestBodyJs["transaction"]['status_detail'];
+$transaction_id = $requestBodyJs["transaction"]['id'];
+$authorization_code = $requestBodyJs["transaction"]['authorization_code'];
+$dev_reference = $requestBodyJs["transaction"]['dev_reference'];
+$payment_message = $requestBodyJs["transaction"]['message'];
+
+$order = new WC_Order($dev_reference);
+$status_order = $order->get_status();
+
+if (!in_array($status_order, ['completed', 'cancelled', 'refunded', 'processing'])) {
+    $description = __("Payment Response: ", "pg_woocommerce") .
+        __(" | Status: ", "pg_woocommerce") . $status .
+        __(" | Status_detail: ", "pg_woocommerce") . $status_detail .
+        __(" | Dev_Reference: ", "pg_woocommerce") . $dev_reference .
+        __(" | Authorization_Code: ", "pg_woocommerce") . $authorization_code .
+        __(" | Transaction_Code: ", "pg_woocommerce") . $transaction_id;
+
+    if ($status_detail == 3) {
+        $comments = __("Successful Payment", "pg_woocommerce");
+        $order->update_status('processing');
+        $order->reduce_order_stock();
+        $order->add_order_note( __('The payment has been made successfully. Transaction Code: ', 'pg_woocommerce') . $transaction_id . __(' and its Authorization Code is: ', 'pg_woocommerce') . $authorization_code);
+    } elseif (in_array($status_detail, [0, 1, 31, 35, 36])) {
+        $comments = __("Pending Payment", "pg_woocommerce");
+        $order->update_status('on-hold');
+        $order->add_order_note( __('The payment is pending.', 'pg_woocommerce') . $transaction_id . __(' the reason is: ', 'pg_woocommerce') . $payment_message);
+    } elseif (in_array($status_detail, [7, 34, 21, 22, 23, 24, 25, 26, 27, 28, 29])) {
+        $order->update_status('refunded');
+        $order->add_order_note( __('Transaction refunded: ', 'pg_woocommerce') . $transaction_id . __(' status: ', 'pg_woocommerce') . $payment_message);
+    } elseif ($status_detail == 8) {
+        $description = "Chargeback";
+        $comments = __("Payment Cancelled", "pg_woocommerce");
+        $order->update_status('cancelled');
+        $order->add_order_note( __('The payment was cancelled. Transaction Code: ', 'pg_woocommerce') . $transaction_id . __(' the reason is chargeback. ', 'pg_woocommerce'));
+    } else {
+        $comments = __("Failed Payment", "pg_woocommerce");
+        $order->update_status('failed');
+        $order->add_order_note( __('The payment has failed. Transaction Code: ', 'pg_woocommerce') . $transaction_id . __(' the reason is: ', 'pg_woocommerce') . $payment_message);
+    }
+}
+
+PG_WC_Helper::insert_data($status, $comments, $description, $dev_reference, $transaction_id);
